d646aceea8d95158c6b1bed111df7aedd4a917e4
agostinoluca <agostino.luca89@gmail.com>
2024-02-07 21:09:42 +0100

    add axios add request add console.log

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,5 +1,23 @@
 <script>
+import axios from 'Axios'
 export default {
+    name: 'AppMain',
+    data() {
+        return {
+            api_url: 'https://db.ygoprodeck.com/api/v7/cardinfo.php',
+            error: false,
+        }
+    },
+    mounted() {
+        axios
+            .get(this.api_url)
+            .then((response) => {
+                console.log(response.data);
+            })
+            .catch((error) => {
+                console.error(error);
+            })
+    }
 
 }
 </script>
