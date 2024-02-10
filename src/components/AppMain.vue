776d85af173a18186311823547e05020af0ddad7
agostinoluca <agostino.luca89@gmail.com>
2024-02-10 19:32:21 +0100

    add characters in page

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -4,8 +4,9 @@ export default {
     name: 'AppMain',
     data() {
         return {
-            api_url: 'https://db.ygoprodeck.com/api/v7/cardinfo.php',
+            api_url: 'https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0',
             error: false,
+            characters: [],
         }
     },
     mounted() {
@@ -13,6 +14,8 @@ export default {
             .get(this.api_url)
             .then((response) => {
                 console.log(response.data);
+                this.characters = response.data.data;
+                console.log(this.characters);
             })
             .catch((error) => {
                 console.error(error);
@@ -24,7 +27,14 @@ export default {
 
 <template>
     <main>
-        <h1>main</h1>
+        <div class="container">
+            <div class="row" v-for="character in characters">
+                <div class="col-12">
+                    <img :src="character.card_images[0].image_url" alt="">
+                    {{ character.name }}
+                </div>
+            </div>
+        </div>
     </main>
 </template>
 
