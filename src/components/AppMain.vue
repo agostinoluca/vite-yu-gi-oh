388a309112c941819b8f96ee01f67d552a34a8bc
agostinoluca <agostino.luca89@gmail.com>
2024-02-11 22:00:18 +0100

    add filter

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -4,10 +4,11 @@ export default {
     name: 'AppMain',
     data() {
         return {
-            api_url: 'https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0',
+            api_url: 'https://db.ygoprodeck.com/api/v7/cardinfo.php?num=150&offset=0',
             error: false,
             characters: [],
-            select_options: []
+            select_options: [],
+            selected_option: 'All',
         }
     },
     mounted() {
@@ -25,8 +26,12 @@ export default {
             .catch((error) => {
                 console.error(error);
             })
+    },
+    computed: {
+        filteredCharacters() {
+            return this.selected_option === 'All' ? this.characters : this.characters.filter(character => character.archetype === this.selected_option);
+        }
     }
-
 }
 </script>
 
@@ -35,21 +40,20 @@ export default {
         <div class="container">
             <div class="row">
                 <div class="col-12 m-1 mb-3">
-                    <select class="form-select" name="filter" id="filter" style="width: auto;">
+                    <select class="form-select" name="filter" id="filter" style="width: auto;" v-model="selected_option">
                         <option value="All">All Cards Type</option>
                         <option v-for="option in select_options" :value="option">{{ option }}
                         </option>
                     </select>
                 </div>
-                <div class="col-12 col-sm-6 col-md-4 col-lg-2" v-for="character in characters"
+                <div class="col-12 col-sm-6 col-md-4 col-lg-2" v-for="character in filteredCharacters"
                     :key="character.id + '_character'">
                     <div class="card text-center m-1 p-2">
                         <img :src="character.card_images[0].image_url" :alt="character.name + ' image'">
-                        <div class="card-body">
+                        <div class="card-body d-flex flex-column justify-content-between">
                             <h5 class="card-title"> {{ character.name }}
                             </h5>
-                            <h6 class="card-subtitle mb-2">{{ character.type }}</h6>
-                            <p>{{ character.archetype }}</p>
+                            <h6 class="card-subtitle mb-2">{{ character.archetype }}</h6>
                         </div>
                     </div>
                 </div>
@@ -70,6 +74,15 @@ export default {
 
     & img {
         border-radius: 0.5rem;
+    }
+
+    & h5 {
+        text-transform: uppercase;
+    }
+
+    & h6 {
+        color: rgb(192, 192, 192);
+        font-weight: 400;
     }
 }
 </style>
