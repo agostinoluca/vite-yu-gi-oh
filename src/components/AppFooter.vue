57f4ce9fca4c315f80be1074fe1cb32ae53dba32
agostinoluca <agostino.luca89@gmail.com>
2024-02-11 20:23:34 +0100

    add footer content

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -6,9 +6,25 @@ export default {
 
 <template>
     <footer>
-        <h1>footer</h1>
+        <div class="container d-flex justify-content-center">
+            <p>Design by a Boolean® student (created in 2024)</p>
+        </div>
     </footer>
 </template>
 
 
-<style scoped></style>
+<style scoped>
+footer {
+    background-color: rgba(0, 0, 0, 0.877);
+    height: 3rem;
+    margin-top: 1rem;
+    padding: 1rem;
+}
+
+p {
+    color: white;
+    font-size: small;
+    font-weight: 100;
+    font-style: italic;
+}
+</style>
