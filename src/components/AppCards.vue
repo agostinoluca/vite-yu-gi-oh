8ae1ea090d32548d1870ca1a3bb0b49a6759bc42
agostinoluca <agostino.luca89@gmail.com>
2024-02-12 21:07:18 +0100

    add AppCards component

---
+++ src/components/AppCards.vue
@@ -0,0 +1,51 @@
+<script>
+export default {
+    name: 'AppCards',
+    props: {
+        characters: {
+            type: Array,
+            required: true
+        }
+    }
+
+}
+</script>
+        
+<template>
+    <div class="col-12 col-sm-6 col-md-4 col-lg-2" v-for="character in characters" :key="character.id + '_character'">
+        <div class="card text-center mb-3">
+            <img :src="character.card_images[0].image_url" :alt="character.name + ' image'">
+            <div class="card-body d-flex flex-column justify-content-between">
+                <h5 class="card-title"> {{ character.name }}
+                </h5>
+                <h6 class="card-subtitle mb-2">{{ character.archetype }}</h6>
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<style scoped>
+.card {
+    background-color: rgba(0, 0, 0, 0.418);
+    box-shadow: 0 0 0.25rem black;
+
+    & .card-body {
+        height: 10rem;
+        color: white;
+    }
+
+    & img {
+        border-radius: 0.5rem;
+    }
+
+    & h5 {
+        text-transform: uppercase;
+    }
+
+    & h6 {
+        color: rgb(192, 192, 192);
+        font-weight: 400;
+    }
+}
+</style>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,10 +1,12 @@
 <script>
 import axios from 'Axios'
 import AppLoader from './AppLoader.vue'
+import AppCards from './AppCards.vue'
 export default {
     name: 'AppMain',
     components: {
-        AppLoader
+        AppLoader,
+        AppCards
     },
     data() {
         return {
@@ -23,11 +25,11 @@ export default {
                 .then((response) => {
                     this.characters = response.data.data;
                     this.loading = false;
-                    console.log(response.data.data[0].archetype);
+                    // console.log(response.data.data[0].archetype);
                     this.characters.forEach(character => {
                         if (character.archetype && !this.select_options.includes(character.archetype)) {
                             this.select_options.push(character.archetype);
-                            console.log(character.archetype);
+                            // console.log(character.archetype);
                         }
                     });
                 })
@@ -70,17 +72,7 @@ export default {
                         {{ getResults }}
                     </div>
                 </div>
-                <div class="col-12 col-sm-6 col-md-4 col-lg-2" v-for="character in filteredCharacters"
-                    :key="character.id + '_character'">
-                    <div class="card text-center mb-3">
-                        <img :src="character.card_images[0].image_url" :alt="character.name + ' image'">
-                        <div class="card-body d-flex flex-column justify-content-between">
-                            <h5 class="card-title"> {{ character.name }}
-                            </h5>
-                            <h6 class="card-subtitle mb-2">{{ character.archetype }}</h6>
-                        </div>
-                    </div>
-                </div>
+                <AppCards :characters="filteredCharacters"></AppCards>
             </div>
             <AppLoader v-else></AppLoader>
         </div>
@@ -99,28 +91,5 @@ main {
     padding: 0.5rem;
     border-radius: 1rem;
     box-shadow: 0 0 0.15rem rgba(255, 255, 255, 0.356);
-}
-
-.card {
-    background-color: rgba(0, 0, 0, 0.418);
-    box-shadow: 0 0 0.25rem black;
-
-    & .card-body {
-        height: 10rem;
-        color: white;
-    }
-
-    & img {
-        border-radius: 0.5rem;
-    }
-
-    & h5 {
-        text-transform: uppercase;
-    }
-
-    & h6 {
-        color: rgb(192, 192, 192);
-        font-weight: 400;
-    }
 }
 </style>
